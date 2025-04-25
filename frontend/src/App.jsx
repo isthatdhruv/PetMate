31eec5db851004d42115bf4da42e4ee79c0a6a2d
Abhi670982 <akchauhan1172@gmail.com>
2025-04-25 13:53:05 +0530

    landing page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import HomePage from "./components/HomePage.jsx";
+
+const App = () => {
+  return <HomePage />;
+};
+
+export default App;
